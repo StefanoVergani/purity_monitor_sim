784d95cafee045c50a90ce6bad7b00710110923d
StefanoVergani <s.vergani@ucl.ac.uk>
2022-08-09 10:17:41 +0100

    cleaned code a tiny bit

--- PuritySimTools.py
+++ PuritySimTools.py
@@ -17,7 +17,6 @@ class PuritySimTools:
     #this function takes as input a list with the capacities and sums them in series
     @staticmethod
     def C_in_series(input_list):
-        #return sum(input_list)/np.prod(np.array(input_list))
         new_list = []
         for i in range(len(input_list)):
             new_list.append(pow(input_list[i],-1))
